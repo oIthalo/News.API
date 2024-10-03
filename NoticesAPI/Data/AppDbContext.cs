3d35682a6d0a943c1c33d061ab7afb136cd95344
oIthalo <ithalobarreto333@gmail.com>
2024-10-03 18:01:29 -0300

    Database configured

---
+++ NoticesAPI/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace NoticesAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opts)
+        : base(opts) { }
+}
